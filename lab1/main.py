0f19f0ef88f0649c2f5f15f057491674e7caeae7
beesaiboo <beedzibood@gmail.com>
2024-02-16 03:15:08 +0300

    created test.csv again and printed data from test.csv in main.py

--- lab1/main.py
+++ lab1/main.py
@@ -1,4 +1,7 @@
 import csv
 
 if __name__ == '__main__':
-    print("First commit")
+    with open("test.csv", encoding='utf-8') as r_file:
+        data = csv.reader(r_file, delimiter=',')
+        for row in data:
+            print(*row)
