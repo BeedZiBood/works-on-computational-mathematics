82ab9d77bac59417abf024c29ef76fb6afd94368
beesaiboo <beedzibood@gmail.com>
2024-02-16 11:57:05 +0300

    fixed test.csv and changed data: replaced the object reader with DictReader

--- lab1/main.py
+++ lab1/main.py
@@ -2,6 +2,6 @@ import csv
 
 if __name__ == '__main__':
     with open("test.csv", encoding='utf-8') as r_file:
-        data = csv.reader(r_file, delimiter=',')
+        data = csv.DictReader(r_file, delimiter=',')
         for row in data:
-            print(*row)
+            print(row["x"], row['f'])
