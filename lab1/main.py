6c0d3114f6d2412e7cab67fa43981e5b24129a76
beesaiboo <beedzibood@gmail.com>
2024-02-18 21:35:30 +0300

    Used polynomial of Lagrange and created a plot with it

--- lab1/main.py
+++ lab1/main.py
@@ -1,7 +1,15 @@
-import csv
+import pandas as pd
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    with open("test.csv", encoding='utf-8') as r_file:
-        data = csv.DictReader(r_file, delimiter=',')
-        for row in data:
-            print(row["x"], row['f'])
+    data = pd.read_csv("test.csv")
+    x = np.array(data['x'])
+    f = np.array(data['f'])
+    lagrange = sp.interpolate.lagrange(x, f)
+    print(lagrange)
+    print(lagrange.coef)
+    x_new = np.arange(-1, 0.9, 0.1)
+    plt.plot(x_new, lagrange(x_new), label='Lagrange')
+    plt.show()
