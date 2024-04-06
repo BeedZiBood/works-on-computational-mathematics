5872c11c15f4d044d82f35c3efc12d9f548b069d
beesaiboo <beedzibood@gmail.com>
2024-04-07 02:55:09 +0300

    Tested RKF45 in main.cpp

---
+++ lab3/Function_for_laboratory.h
@@ -0,0 +1,8 @@
+#ifndef LAB3_FUNCTION_FOR_LABORATORY_H
+#define LAB3_FUNCTION_FOR_LABORATORY_H
+
+namespace mashkin
+{
+  void func(double t, double* x, double* dx);
+}
+#endif
--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,4 +1,23 @@
+#include <iostream>
+#include "RKF45.h"
+#include "Function_for_laboratory.h"
+
 int main()
 {
+  double x[2]{3.0, -1.0};
+  double t(0);
+  double tOut(0);
+  double relerr(0.0001);
+  double abserr(0.0001);
+  double work[3+6*2];
+  int flag = 1;
+  int line = 1;
+  for (double i = 0.0075; i <= 0.1501; i += 0.0075)
+  {
+    tOut = i;
+    std::cout << line++ << " ";
+    RKF45(mashkin::func, 2, x, t, tOut, relerr, abserr, work, flag);
+    std::cout << t << " " << tOut << " " << x[0] << " " << x[1] << " flag = " << flag << "\n";
+  }
   return 0;
 }
