b64c588061781a15b89cff6ad9a073dbccc85530
beesaiboo <beedzibood@gmail.com>
2024-04-14 21:25:21 +0300

    declared function for search min x

---
+++ course_work/functions_for_course_work.cpp
@@ -0,0 +1,5 @@
+//
+// Created by beedz on 14.04.2024.
+//
+
+#include "functions_for_course_work.h"
---
+++ course_work/functions_for_course_work.h
@@ -0,0 +1,8 @@
+#ifndef COURSE_WORK_FUNCTIONS_FOR_COURSE_WORK_H
+#define COURSE_WORK_FUNCTIONS_FOR_COURSE_WORK_H
+
+namespace mashkin
+{
+  void function_for_search_min_x(double x);
+}
+#endif
