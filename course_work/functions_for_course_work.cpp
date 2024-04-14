fdcd771c1794af5b8630bfb20d0df80c49f13456
beesaiboo <beedzibood@gmail.com>
2024-04-14 21:42:21 +0300

    defined function for search min x

--- course_work/functions_for_course_work.cpp
+++ course_work/functions_for_course_work.cpp
@@ -1,5 +1,10 @@
-//
-// Created by beedz on 14.04.2024.
-//
-
 #include "functions_for_course_work.h"
+#include <cmath>
+
+namespace mashkin
+{
+  double function_for_search_min_x(double x)
+  {
+    return  1.0/std::tan(x) + x/(1 - x * x);
+  }
+}
